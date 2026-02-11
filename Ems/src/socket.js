2bf1c700b9b10074936047e69d8739777cbf9caf
Lokesh Kumar <lokeshk3875@gmail.com>
2026-02-11 21:50:43 +0530

    update now

--- Ems/src/socket.js
+++ Ems/src/socket.js
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://ems-backend-jy3w.onrender.com/";
+const SOCKET_URL = "https://ems-backend-jy3w.onrender.com";
 
 const socket = io(SOCKET_URL, {
   withCredentials : true,
-  transports: ["websocket"],
-
+  transports: ["websocket" ,  "polling"],
+   autoConnect: true,
 });
 
 export default socket;
