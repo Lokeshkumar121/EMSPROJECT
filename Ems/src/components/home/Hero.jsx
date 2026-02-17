fecba12d18bf0750c35259ff2f6f27c886fb0270
Lokesh Kumar <lokeshk3875@gmail.com>
2026-02-17 21:07:06 +0530

    update backend

--- Ems/src/components/home/Hero.jsx
+++ Ems/src/components/home/Hero.jsx
@@ -3,25 +3,36 @@ import LiveDate from "../../livedate/LiveDate";
 
 const Hero = () => {
   return (
-    <section className="text-center py-24 px-6">
-       
-                
-      <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
-        Employee Management System
-      </h1>
+    <section
+      className="relative text-center py-24 px-6 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url('https://cdn.prod.website-files.com/6209ea9aee1f965d7fce7c19/649a89c89a17e113b6f49621_empployee%20mngment%20app%20(1).jpg')",
+      }}
+    >
+      {/* Black Overlay */}
+      <div className="absolute inset-0 bg-black/70"></div>
 
-      <p className="text-gray-400 max-w-2xl mx-auto text-lg">
-        Manage employees, assign tasks, track progress and boost productivity —
-        all in one powerful dashboard.
-      </p>
-      <div className="mt-10 flex justify-center gap-4">
-        <button className="px-6 py-3 bg-emerald-600 rounded-xl font-semibold hover:bg-emerald-700">
-          Get Started
-        </button>
+      {/* Content */}
+      <div className="relative z-10">
+        <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
+          Employee Management System
+        </h1>
 
-        <button className="px-6 py-3 border border-gray-600 rounded-xl text-gray-300 hover:bg-gray-800">
-          Learn More
-        </button>
+        <p className="text-gray-400 max-w-2xl mx-auto text-lg">
+          Manage employees, assign tasks, track progress and boost productivity —
+          all in one powerful dashboard.
+        </p>
+
+        <div className="mt-10 flex justify-center gap-4">
+          <button className="px-6 py-3 bg-emerald-600 rounded-xl font-semibold hover:bg-emerald-700">
+            Get Started
+          </button>
+
+          <button className="px-6 py-3 border border-gray-600 rounded-xl text-gray-300 hover:bg-gray-800">
+            Learn More
+          </button>
+        </div>
       </div>
     </section>
   );
