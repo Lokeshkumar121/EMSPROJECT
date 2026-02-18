4435da953c6ac63b26e10efd77423903a72e200f
Lokesh Kumar <lokeshk3875@gmail.com>
2026-02-18 23:23:38 +0530

    update

--- Ems/src/context/AuthProvider.jsx
+++ Ems/src/context/AuthProvider.jsx
@@ -60,7 +60,7 @@ const AuthProvider = ({ children }) => {
         err.response?.data || err.message
       );
     }
-    fetchEmployees(); // instant UI update
+    // fetchEmployees(); // instant UI update
   };
 
     // ➕ ADD TASK (BACKEND)
