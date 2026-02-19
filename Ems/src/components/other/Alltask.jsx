938c9c4363ffa99e419406c6799b99e8aba46169
Lokesh Kumar <lokeshk3875@gmail.com>
2026-02-19 11:00:39 +0530

    update

--- Ems/src/components/other/Alltask.jsx
+++ Ems/src/components/other/Alltask.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import socket from "../../socket";
 import { API_BASE } from "../../config/api";
+import { toast } from "react-toastify";
 
 const Alltask = ({ employees, setEmployees, onEmployeeDeleted }) => {
   const navigate = useNavigate();
